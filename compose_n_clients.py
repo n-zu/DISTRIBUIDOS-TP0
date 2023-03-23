cd4d15d92383e8b7ec52b4371ace04b31f122afd
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-03-23 12:55:39 -0300

    N1.1

---
+++ compose_n_clients.py
@@ -0,0 +1,49 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 compose_n_clients.py n")
+    sys.exit(1)
+
+clients = int(sys.argv[1])
+
+yaml = '''
+version: "3.9"
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+'''
+
+for i in range(clients):
+    yaml += f'''
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+'''
+
+yaml += '''
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+'''
+
+with open("docker-compose-dev.yaml", "w") as f:
+    f.write(yaml)
