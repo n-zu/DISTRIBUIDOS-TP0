295e43f2b7bda7a0ab9fed379f0dd78de95306a5
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-03-23 17:13:38 -0300

    N4-Server

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,7 @@
 import socket
 import logging
+import signal
+import sys
 
 
 class Server:
@@ -8,20 +10,39 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self.clients = []
+
+        # Define signal handler to gracefully shutdown the server
+        signal.signal(signal.SIGINT, self.__signal_handler)
+        signal.signal(signal.SIGTERM, self.__signal_handler)
+
+    def __signal_handler(self, signum, stack):
+        """
+        Signal handler to gracefully shutdown the server
+
+        """
+        logging.info(
+            f"action: signal_handler | result: success | signal: {signum}")
+        self._server_socket.close()
+        logging.debug(f'action: close listener socket | result: success')
+        for client in self.clients:
+            client.close()
+            logging.debug(f'action: close client socket | result: success')
+
+        sys.exit(0)
 
     def run(self):
         """
         Dummy Server loop
 
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
+        Server that accept a new connections and establishes a communication with a client.
+        After client's communication finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
+        # TODO: Modify this program to handle signal to graceful shutdown the server when multithreading
         while True:
             client_sock = self.__accept_new_connection()
+            self.clients.append(client_sock)
             self.__handle_client_connection(client_sock)
 
     def __handle_client_connection(self, client_sock):
@@ -35,13 +56,16 @@ class Server:
             # TODO: Modify the receive to avoid short-reads
             msg = client_sock.recv(1024).rstrip().decode('utf-8')
             addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
+            logging.info(
+                f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
             # TODO: Modify the send to avoid short-writes
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(
+                "action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
+            self.clients.remove(client_sock)
 
     def __accept_new_connection(self):
         """
@@ -54,5 +78,6 @@ class Server:
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        logging.info(
+            f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
